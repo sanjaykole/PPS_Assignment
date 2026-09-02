d93e004651bc7d10a4b0db34a9dd20faf611fb5b
sanjaykole <sanjaykole1007@gmail.com>
2026-09-02 09:31:12 +0530

    Update Ass 5.py

--- Ass 5.py
+++ Ass 5.py
@@ -12,14 +12,14 @@ top_performers = df.loc[df['Join_year'] <= 2020,['Department', 'Salary']]
 
 import numpy as np
 df['Bonus_Eligible'] = np.where(df['Join_year'] <= 2022,'Yes', 'No')
-print("======================================================================")
+print("======= Filtered: High Earning Engineers ======")
 print(high_earning_engineers)
 
-print("======================================================================")
+print("====== Sorted: BY Rating & Salary(Descending) ======")
 print(sorted_df)
 
-print("======================================================================")
+print("====== Conditional Selection: Top Performers(Rating>=4.0) ======")
 print(top_performers)
 
-print("======================================================================")
+print("====== Final DataFrame With Conditional 'Bonus Eligible' Column ======")
 print(df)
